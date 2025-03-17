cefd68b256f9bea2c76fd08b46d5ff393dde17d3
DiamondSssssss <trongtri140604@gmail.com>
2025-03-17 10:42:07 +0700

    opening

--- src/components/Opening/Opening.jsx
+++ src/components/Opening/Opening.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./Opening.scss";
+import { RotateCcw } from "lucide-react";
 const Opening = () => {
-  return <div>Opening</div>;
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-text">Welcome</h1>
+      <div className="reload-icon">
+        <RotateCcw size={50} color="yellow" />
+      </div>
+    </div>
+  );
 };
 
 export default Opening;
